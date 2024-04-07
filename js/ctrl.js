16a9bfe4460397d979b358020de656386be4256f
ngthibaothi000 <145281029+ngthibaothi000@users.noreply.github.com>
2024-04-07 17:18:36 +0700

    dang nhap va dang ki

--- js/ctrl.js
+++ js/ctrl.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
     $('#eye').click(function(){
-      var inputField = $(this).prev(); // Trường nhập mật khẩu trước đó
+      var boxField = $(this).prev(); // Trường nhập mật khẩu trước đó
       var icon = $(this).find('i'); // Icon mắt
-      if (inputField.attr('type') === 'password') { // show
+      if (boxField.attr('type') === 'password') { // show
         // Nếu mật khẩu đang ẩn, hiển thị mật khẩu và chuyển icon sang "mắt đóng"
-        inputField.attr('type', 'text');
+        boxField.attr('type', 'text');
         icon.addClass('fa-eye');
         icon.removeClass('fa-eye-slash');
       } else {
         // Ngược lại, ẩn mật khẩu và chuyển icon sang "mắt mở"
-        inputField.attr('type', 'password');
+        boxField.attr('type', 'password');
         icon.addClass('fa-eye-slash');
         icon.removeClass('fa-eye');
       }
