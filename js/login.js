b99f4a7ab50548cb5dc43124c11e3b1a2d373efe
nlthanhdanh <145256154+danh1215@users.noreply.github.com>
2024-04-09 01:14:17 +0700

    them tinh nang an hien mat khau

--- js/login.js
+++ js/login.js
@@ -3,18 +3,31 @@ const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
-registerLink.addEventListener('click', ()=> {
-  wrapper.classList.add('active');
+
+// JavaScript để chuyển đổi hiển thị mật khẩu
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+
+togglePassword.addEventListener('click', function (e) {
+    // Chuyển đổi kiểu của trường nhập mật khẩu
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // Chuyển đổi icon
+    this.classList.toggle('fa-eye-slash');
+})
+
+registerLink.addEventListener('click', () => {
+    wrapper.classList.add('active');
 });
 
-loginLink.addEventListener('click', ()=> {
+loginLink.addEventListener('click', () => {
     wrapper.classList.remove('active');
 });
 
-btnPopup.addEventListener('click', ()=> {
+btnPopup.addEventListener('click', () => {
     wrapper.classList.add('active-popup');
 });
 
-iconClose.addEventListener('click', ()=> {
+iconClose.addEventListener('click', () => {
     wrapper.classList.remove('active-popup');
 });
